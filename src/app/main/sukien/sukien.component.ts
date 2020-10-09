c0efd81f4cb86ab67a66e01e2b1082845482b171
tungxuan99 <70497455+tungxuan99@users.noreply.github.com>
2020-10-09 23:07:04 +0700

    update 9/10

--- src/app/main/sukien/sukien.component.ts
+++ src/app/main/sukien/sukien.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from 'src/app/lib/base-component';
+import { Component, OnInit,Injector } from '@angular/core';
+import { Observable} from 'rxjs';
+import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/operator/takeUntil';
 
 @Component({
   selector: 'app-sukien',
   templateUrl: './sukien.component.html',
   styleUrls: ['./sukien.component.css']
 })
-export class SukienComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class SukienComponent extends BaseComponent implements OnInit {
+  list_tintuc:any;
+  constructor(injector: Injector) { 
+    super(injector);
   }
 
+  ngOnInit(): void {
+    Observable.combineLatest(
+      this._api.get('/api/tintuc/get-all'),
+    ).takeUntil(this.unsubscribe).subscribe(res => {
+      this.list_tintuc = res[0];
+      setTimeout(() => {
+        this.loadScripts();
+      });
+    }, err => { });
+  }
 }
+
